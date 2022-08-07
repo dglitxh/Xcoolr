d870d212f9d8e33a434fa2082b1e22591383f118
ydeezly <ydglitch@gmail.com>
2022-08-07 12:15:58 +0000

    upd: config

--- config/config.ts
+++ config/config.ts
@@ -1,12 +1,18 @@
 const redis = require("redis")
 
-const client = redis.createClient({
+
+const redisClient = redis.createClient({
     socket: {
         host: 'localhost',
         port: 6379
     },
-    password: process.env.REDIS_PWD
 });
-client.on('error', (err: Error) => {
+redisClient.on('error', (err: Error) => {
     console.log('Error ' + err);
 });
+
+redisClient.on('connect', function (err: Error) {
+    console.log('Connected to redis successfully');
+});
+
+module.exports = redisClient
