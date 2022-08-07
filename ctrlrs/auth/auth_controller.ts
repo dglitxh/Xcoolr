0fa8fa2384ff029283641e305989a2128468d255
ydeezly <ydglitch@gmail.com>
2022-08-07 12:16:21 +0000

    upd: auth ctrlr

--- ctrlrs/auth/auth_controller.ts
+++ ctrlrs/auth/auth_controller.ts
@@ -1,27 +1,24 @@
 import { Router } from "express";
-import passportCustom from 'passport-custom';
-const CustomStrategy = passportCustom.Strategy;
+import passport from "passport"
+const redisClient = require("../../config/config")
 
 
 require("dotenv").config()
-const passport = require("passport")
 const session = require("express-session")
 const router = Router()
-const t_login = require("./tutor/t_login")(passport)
-const t_signUp = require("./tutor/signUp")(passport)
+const t_login = require("./tutor/t_login")
+const t_signUp = require("./tutor/signUp")
+const connectRedis = require("connect-redis")
+const RedisStore = session(connectRedis)
 
+router.use(session({ secret: "67ygghiuoh", resave: false, saveUninitialized: false,
+}))
 
-router.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized:true}))
-router.use(passport.initialize());
-router.use(passport.session());
 router.post("/tutor/signup", t_signUp)
-router.post("/tutor/login", passport.authenticate("custom", {
-    successRedirect: "/",
-    failureRedirect: "/"
-  }))
+router.post("/tutor/login", t_login)
 router.get("/yd", (req, res) => {
     res.json({"yhh": "get me lit"})
 })
-passport.use("/tutor/login", new CustomStrategy(t_login))
+
 
 module.exports = router
