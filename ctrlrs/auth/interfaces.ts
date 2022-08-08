8872d313c717acb2c830ac745194870873ca4821
ydeezly <ydglitch@gmail.com>
2022-08-08 17:14:03 +0000

    upd: interface

--- ctrlrs/auth/interfaces.ts
+++ ctrlrs/auth/interfaces.ts
@@ -8,3 +8,13 @@ module.exports; interface log_in {
     email: string;
     password: string;
 }
+
+module.exports; interface t_profile {
+    id       :number
+    bio      :string
+    teacher  :object   
+    teacherId:number       
+    subjects :object[]
+    rating   :number
+    Ratings  :object[]
+}
