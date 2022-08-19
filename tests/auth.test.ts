07072559a088469242ef0ba8af9510538e3b10b7
ydeezly <ydglitch@gmail.com>
2022-08-19 21:27:57 +0000

    upd: tests

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -40,5 +40,97 @@ describe("Tests student auth", () => {
 
   });
 
+  test("It should create new profile for student", async () => {
+    const response = await request(app)
+        .post("/auth/student/1/profile/create")
+        .send({
+            bio: "we the best brody fr",
+          })
+    expect(response.statusCode).toBe(200);
+
+  });
+
+  test("It should create get profile with student id", async () => {
+    const response = await request(app)
+        .get("/auth/student/1/profile")
+    expect(response.statusCode).toBe(200);
+  });
+ 
+  test("It should update student profile", async () => {
+    const response = await request(app)
+        .put("/auth/student/1/profile/update")
+        .send({
+            bio: "we the new best brody fr",
+          })
+    expect(response.statusCode).toBe(200);
+  });
+
+    test("It should delete student profile by id", async () => {
+      const response = await request(app)
+          .get("/auth/student/1/profile/delete")
+      expect(response.statusCode).toBe(200);
+  
+    });
+
+});
+
+
+describe("Tests teacher auth", () => {
+  const app = require("../index");
+
+  test("It should respond to the Sign Up POST method", async () => {
+    const response = await request(app)
+        .post("/auth/tutor/signup")
+        .send({
+            name: "ydeezus",
+            email: 'daboii@gmail.com',
+            password: "trickdaddy"
+          })
+    expect(response.statusCode).toBe(200);
+
+  });
+
+  test("It should respond to the login POST method", async ()  => {
+    const response = await request(app)
+        .post("/auth/tutor/login")
+        .send({
+            email: 'daboii@gmail.com',
+            password: "trickdaddy"
+          })
+    expect(response.statusCode).toBe(200);
+  
+  });
+
+  test("It should create new profile for teacher", async () => {
+    const response = await request(app)
+        .post("/auth/tutor/1/profile/create")
+        .send({
+            bio: "we the best brody fr",
+          })
+    expect(response.statusCode).toBe(200);
+
+  });
+
+  test("It should get profile with teacher id", async () => {
+    const response = await request(app)
+        .get("/auth/tutor/1/profile")
+    expect(response.statusCode).toBe(200);
+  });
+ 
+  test("It should update teacher profile", async () => {
+    const response = await request(app)
+        .put("/auth/tutor/1/profile/update")
+        .send({
+            bio: "we the new best brody fr",
+          })
+    expect(response.statusCode).toBe(200);
+  });
+
+    test("It should delete teacher profile by id", async () => {
+      const response = await request(app)
+          .get("/auth/tutor/1/profile/delete")
+      expect(response.statusCode).toBe(200);
+  
+    });
 
 });
