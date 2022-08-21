98dc5ce4b60c2e3f91717db290d517d7768e737e
ydeezly <ydglitch@gmail.com>
2022-08-21 20:40:33 +0000

    upd: ind

--- index.ts
+++ index.ts
@@ -4,25 +4,19 @@ const authrouter = require("./ctrlrs/auth/auth_controller")
 const apiRouter = require("./ctrlrs/controller")
 const app: Application = express()
 import bodyParser from "body-parser";
-import { time, timeStamp } from "console";
-
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.raw())
 app.use(bodyParser.text())
-
 app.use((req, res, next) => {
     console.log(`method: ${req.method}   path: ${req.url}   time: ${Date.now()}`)
     next()
 })
-
 app.use("/auth", authrouter)
-app.use("/api/v1")
+app.use("/api/v1", apiRouter)
 app.get("/", (req, res) => {
-    res.status(200).json({"msg": "Welcome to Xcoolr"})
-    
+    res.status(200).json({"msg": "Welcome to Xcoolr"}) 
 })
 
 
