2b4b14a76aa29169b0977665ebc475d5725ef229
ydeezly <ydglitch@gmail.com>
2022-08-22 18:27:11 +0000

    upd: controllers

--- ctrlrs/controller.ts
+++ ctrlrs/controller.ts
@@ -8,9 +8,9 @@ const getSub = require("./subjects/getSub")
 const getAllSubs = require("./subjects/getSubjects")
 
 
-router.post("/subjects/:id/create", newSub)
-router.put("/subjects/:id/update", updSub)
-router.get("/subjects/:id/delete", delSub)
+router.post("/subjects/tutor/:id/create", newSub)
+router.put("/subjects/tutor/:id/update", updSub)
+router.get("/subjects/tutor/:id/delete", delSub)
 router.get("/subjects/:id", getSub)
 router.get("/subjects", getAllSubs)
 
