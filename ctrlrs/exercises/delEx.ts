d0036d5223201fff5ae984721fa161b0e3bf132e
ydeezly <ydglitch@gmail.com>
2022-08-27 12:31:00 +0000

    delete ex

---
+++ ctrlrs/exercises/delEx.ts
@@ -0,0 +1,26 @@
+
+import { PrismaClient } from "@prisma/client"
+import { Request, Response } from "express"
+
+const prisma = new PrismaClient()
+
+const delEx  = async (req: Request, res: Response): Promise<void> => {
+        try{
+          const id = Number(req.params.id)
+          const exercise = await prisma.exercise.delete({
+              where: {
+                id: id
+              },
+            })
+
+                res.status(200).json({"result": "Exercise deleted succesfully"})
+             }
+          catch(e) {
+            res.status(403).json({"status": "Exercise could not be deleted due to an error"})
+          }
+          finally{
+            prisma.$disconnect()
+          }
+    }
+      
+  module.exports = delEx
