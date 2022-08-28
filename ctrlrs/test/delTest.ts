0624239d222e24a7ea84dc104d2a834bd643940d
ydeezly <ydglitch@gmail.com>
2022-08-28 11:19:01 +0000

    del test

---
+++ ctrlrs/test/delTest.ts
@@ -0,0 +1,26 @@
+
+import { PrismaClient } from "@prisma/client"
+import { Request, Response } from "express"
+
+const prisma = new PrismaClient()
+
+const delTest  = async (req: Request, res: Response): Promise<void> => {
+        try{
+          const id = Number(req.params.id)
+          const test = await prisma.test.delete({
+              where: {
+                id: id
+              },
+            })
+
+                res.status(200).json({"result": "test deleted succesfully"})
+             }
+          catch(e) {
+            res.status(403).json({"status": "test could not be deleted due to an error"})
+          }
+          finally{
+            prisma.$disconnect()
+          }
+    }
+      
+  module.exports = delTest
