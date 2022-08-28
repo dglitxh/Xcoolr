6c0db3b952d6bee4a2d77abf44d8402e5d9fbc1c
ydeezly <ydglitch@gmail.com>
2022-08-28 11:19:30 +0000

    get test

---
+++ ctrlrs/test/getTest.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+const getTest  = async (req: Request, res: Response): Promise<void>  => {
+        try{
+          const id = Number(req.params.id)
+          const test = await prisma.test.findUnique({
+              where: {
+                id: id
+              },
+            })
+           if (!test) {
+               res.status(404).send({"message": "test not found"})   
+           }
+
+                res.send({"result": test})
+             }
+          catch(e) {
+            res.status(400).send("test not found due to an error")
+          }
+          finally{
+            prisma.$disconnect()
+          }
+    }
+      
+  module.exports = getTest
