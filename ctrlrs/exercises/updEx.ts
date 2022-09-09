0f71d7cf92061fd80f6daa9bd1eab50317d4c82a
ydeezly <ydglitch@gmail.com>
2022-09-09 13:30:10 +0000

    upd: upd ex

---
+++ ctrlrs/exercises/updEx.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express"
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+
+const updateExercise = async (req: Request, res: Response): Promise<void>  => {
+    try {
+        const id: number = Number(req.params.id)
+        let exercise = await prisma.exercise.update({
+            where: {
+                id: id
+            },
+            data: {
+                exName: req.body.exName
+              },
+        })
+        if (exercise)
+        res.status(200).send({"message": req.body})
+    }
+    catch(e) {
+        console.log(e)
+        res.status(403).send("Exercise update failed due to an error")
+    }
+    finally{
+        prisma.$disconnect()
+    }
+    }
+
+    module.exports = updateExercise
