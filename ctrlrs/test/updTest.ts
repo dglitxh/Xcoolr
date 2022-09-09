5872abf3d99ee0c7447ca962e0d44b67afe305c3
ydeezly <ydglitch@gmail.com>
2022-09-09 13:31:16 +0000

    upd: upd test

---
+++ ctrlrs/test/updTest.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express"
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+
+const updateTest = async (req: Request, res: Response): Promise<void>  => {
+    try {
+        const id: number = Number(req.params.id)
+        let test = await prisma.test.update({
+            where: {
+                id: id
+            },
+            data: {
+                testName: req.body.testName
+              },
+        })
+        if (test)
+        res.status(200).send({"message": req.body})
+    }
+    catch(e) {
+        console.log(e)
+        res.status(403).send("Test update failed due to an error")
+    }
+    finally{
+        prisma.$disconnect()
+    }
+    }
+
+    module.exports = updateTest
