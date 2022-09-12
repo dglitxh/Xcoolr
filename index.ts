97e888705be6286b9b2500ca00ba087dd3a9948d
ydeezly <ydglitch@gmail.com>
2022-09-12 10:58:56 +0000

    upd: index

--- index.ts
+++ index.ts
@@ -3,8 +3,20 @@ const express = require("express");
 const authrouter = require("./ctrlrs/auth/auth_controller")
 const apiRouter = require("./ctrlrs/controller")
 const app: Application = express()
+const session = require("express-session")
+const redisClient = require("./config/config")
+const cookieParser = require("cookie-parser")
 import bodyParser from "body-parser";
+const redisStore = require('connect-redis')(session);
 
+app.use(cookieParser())
+app.use(session({
+     name: "xcoolr",
+     secret: process.env.SESSION_SECRET, //remember to change this
+     resave: false, 
+     saveUninitialized: false,
+     store: new redisStore({client: redisClient}),
+}))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.raw())
