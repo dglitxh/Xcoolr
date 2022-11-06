053f940ce108e44d06ae04c874f63589f8de04e2
ydeezly <ydglitch@gmail.com>
2022-11-06 18:58:44 +0000

    upd: st test responses

--- ctrlrs/test/addTestScore.ts
+++ ctrlrs/test/addTestScore.ts
@@ -1,46 +1,51 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-interface ex_score  {
-    testId: number
-    s_profileId: number 
-    score: number
+interface ex_score {
+  testId: number;
+  s_profileId: number;
+  score: number;
 }
 
-const addTestScore = async (req: any, res: any): Promise<void>  => {
-    try {
+const addTestScore = async (req: any, res: any): Promise<void> => {
+  try {
     // only teachers can create new ex_scores
-        const body: ex_score = req.body
-        const id: number = Number(req.params.id)
-        const chkScore = await prisma.testScores.findFirst({
-            where: {
-                testId: id,
-                s_profileId: Number(body.s_profileId)
-            }
-        })
+    const body: ex_score = req.body;
+    const id: number = Number(req.params.id);
+    const chkScore = await prisma.testScores.findFirst({
+      where: {
+        testId: id,
+        s_profileId: Number(body.s_profileId),
+      },
+    });
 
-        if (chkScore) res.end("Student has already been scored, try updating score")
+    if (chkScore)
+      res
+        .status(500)
+        .end({
+          status: false,
+          msg: "Student has already been scored, try updating score",
+        });
+    else {
+      const newScore = await prisma.testScores.create({
+        data: {
+          testId: id,
+          s_profileId: Number(body.s_profileId),
+          score: Number(body.score),
+        },
+      });
 
-       else {
-        const newScore = await prisma.testScores.create({
-            data: {
-              testId: id,
-              s_profileId: Number(body.s_profileId),
-              score: Number(body.score)
-            }
-        })
+      res.status(200).send({ status: true, msg: "new test score created" });
+    }
+  } catch (e) {
+    console.log(e);
+    res
+      .status(401)
+      .send({ status: false, msg: "there was an error creating test score" });
+  } finally {
+    prisma.$disconnect();
+  }
+};
 
-        res.status(200).send("new test score created")
-    }
-    }
-    catch(e) {
-        console.log(e)
-        res.status(401).end("there was an error creating test score")
-    }
-    finally{
-        prisma.$disconnect()
-    }
-    }
-
-    module.exports = addTestScore
+module.exports = addTestScore;
--- ctrlrs/test/updTest.ts
+++ ctrlrs/test/updTest.ts
@@ -1,29 +1,27 @@
-import { Request, Response } from "express"
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
+const updateTest = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const id: number = Number(req.params.id);
+    let test = await prisma.test.update({
+      where: {
+        id: id,
+      },
+      data: {
+        testName: req.body.testName,
+      },
+    });
+    if (test) res.status(200).send({ status: true, msg: req.body });
+  } catch (e) {
+    console.log(e);
+    res
+      .status(403)
+      .send({ status: false, msg: "Test update failed due to an error" });
+  } finally {
+    prisma.$disconnect();
+  }
+};
 
-const updateTest = async (req: Request, res: Response): Promise<void>  => {
-    try {
-        const id: number = Number(req.params.id)
-        let test = await prisma.test.update({
-            where: {
-                id: id
-            },
-            data: {
-                testName: req.body.testName
-              },
-        })
-        if (test)
-        res.status(200).send({"message": req.body})
-    }
-    catch(e) {
-        console.log(e)
-        res.status(403).end("Test update failed due to an error")
-    }
-    finally{
-        prisma.$disconnect()
-    }
-    }
-
-    module.exports = updateTest
+module.exports = updateTest;
