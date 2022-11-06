c3cf17d9f99edabcb471b5202e378414b7d46845
ydeezly <ydglitch@gmail.com>
2022-11-06 18:28:29 +0000

    upd: ex responses

--- ctrlrs/exercises/delExScore.ts
+++ ctrlrs/exercises/delExScore.ts
@@ -1,26 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
 
-import { PrismaClient } from "@prisma/client"
-import { Request, Response } from "express"
+const prisma = new PrismaClient();
 
-const prisma = new PrismaClient()
+const delExScore = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const id = Number(req.params.id);
+    const exScore = await prisma.exScores.delete({
+      where: {
+        id: id,
+      },
+    });
 
-const delExScore  = async (req: Request, res: Response): Promise<void> => {
-        try{
-          const id = Number(req.params.id)
-          const exScore = await prisma.exScores.delete({
-              where: {
-                id: id
-              },
-            })
+    res
+      .status(200)
+      .send({ status: true, msg: "Exercise score deleted succesfully" });
+  } catch (e) {
+    res
+      .status(403)
+      .send({
+        status: false,
+        msg: "Exercise score could not be deleted due to an error",
+      });
+  } finally {
+    prisma.$disconnect();
+  }
+};
 
-                res.status(200).send("Exercise score deleted succesfully")
-             }
-          catch(e) {
-            res.status(403).end("Exercise score could not be deleted due to an error")
-          }
-          finally{
-            prisma.$disconnect()
-          }
-    }
-      
-  module.exports = delExScore
+module.exports = delExScore;
--- ctrlrs/exercises/updExScore.ts
+++ ctrlrs/exercises/updExScore.ts
@@ -1,35 +1,37 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-interface ex_score  {
-    score: number
+interface ex_score {
+  score: number;
 }
 
-const updExScore = async (req: any, res: any): Promise<void>  => {
-    try {
+const updExScore = async (req: any, res: any): Promise<void> => {
+  try {
     // only teachers can create new ex_scores
-        const body: ex_score = req.body
-        const id: number = Number(req.params.id)
-        const updScore = await prisma.exScores.update({
-            where: {
-                id: id,
-            },
-            data: {
-              score: Number(body.score)
-            }
-        })
+    const body: ex_score = req.body;
+    const id: number = Number(req.params.id);
+    const updScore = await prisma.exScores.update({
+      where: {
+        id: id,
+      },
+      data: {
+        score: Number(body.score),
+      },
+    });
 
-        res.status(200).end("new exercise score updated")
-    
-    }
-    catch(e) {
-        console.log(e)
-        res.status(401).end("there was an error updating exercise score")
-    }
-    finally{
-        prisma.$disconnect()
-    }
-    }
+    res.status(200).send({ status: true, msg: "new exercise score updated" });
+  } catch (e) {
+    console.log(e);
+    res
+      .status(401)
+      .send({
+        status: false,
+        msg: "there was an error updating exercise score",
+      });
+  } finally {
+    prisma.$disconnect();
+  }
+};
 
-    module.exports = updExScore
+module.exports = updExScore;
