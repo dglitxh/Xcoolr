598829323e7bc2020447aae9bf4413f834315503
ydeezly <ydglitch@gmail.com>
2022-11-06 18:35:16 +0000

    upd: rating response

--- ctrlrs/ratings/delRating.ts
+++ ctrlrs/ratings/delRating.ts
@@ -1,26 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
 
-import { PrismaClient } from "@prisma/client"
-import { Request, Response } from "express"
+const prisma = new PrismaClient();
 
-const prisma = new PrismaClient()
+const delRatings = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const id = Number(req.params.id);
+    const del_rating = await prisma.rating.delete({
+      where: {
+        id: id,
+      },
+    });
 
-const delRatings  = async (req: Request, res: Response): Promise<void> => {
-        try{
-          const id = Number(req.params.id)
-          const del_rating = await prisma.rating.delete({
-              where: {
-                id: id
-              },
-            })
+    res.status(200).send({ status: true, msg: "ratings deleted succesfully" });
+  } catch (e) {
+    res
+      .status(403)
+      .send({
+        status: false,
+        msg: "ratings could not be deleted due to an error",
+      });
+  } finally {
+    prisma.$disconnect();
+  }
+};
 
-                res.status(200).end("ratings deleted succesfully")
-             }
-          catch(e) {
-            res.status(403).end("ratings could not be deleted due to an error")
-          }
-          finally{
-            prisma.$disconnect()
-          }
-    }
-      
-  module.exports = delRatings
+module.exports = delRatings;
