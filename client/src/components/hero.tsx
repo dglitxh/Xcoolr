e60ccf05b07943b63ae2bf5e84e67962110f2711
ydeezly <ydglitch@gmail.com>
2022-11-19 11:02:35 +0000

    upd: hero

--- client/src/components/hero.tsx
+++ client/src/components/hero.tsx
@@ -1,24 +1,24 @@
-import { Container, Grid, Text } from "@nextui-org/react";
-import react from "react";
+import { Container, Text, Row } from "@nextui-org/react";
+import React from "react";
 
 export default function Hero() {
   return (
-    <Container lg>
-      <Grid.Container>
-        <Grid xs={12} sm={6} md={6} lg={6}>
-          <Text h2 b size={65}>
-            The Xcoolr Client
-          </Text>
-          <Text>
-            provides you with the most comprehensive student performance
-            monitoring platform on the market. We are know to deliver insightful
-            and in depth understanding of student's performance.
-          </Text>
-        </Grid>
-        <Grid xs={12} sm={6} md={6} lg={6}>
-          <img src={require("")} />
-        </Grid>
-      </Grid.Container>
+    <Container sm>
+      {/* <Grid.Container>
+        <Grid xs={12} sm={6} md={6} lg={6} justify="center"> */}
+      <Row>
+        <Text h2 b size={65}>
+          The Xcoolr Client
+        </Text>
+      </Row>
+      <Row>
+        <Text>
+          provides you with the most comprehensive student performance
+          monitoring platform on the market. We are know to deliver insightful
+          and in depth understanding of student's performance in the most
+          interactive way possible.
+        </Text>
+      </Row>
     </Container>
   );
 }
