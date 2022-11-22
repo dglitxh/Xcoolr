a62fdd983f423490691a0fd59b55aec192eb8ad4
ydeezly <ydglitch@gmail.com>
2022-11-22 22:50:28 +0000

    add: card sect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "./components/navbar";
 import Hero from "./components/hero";
+import CardSection from "./components/cardSect";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Hero />
+      <CardSection />
     </div>
   );
 }
