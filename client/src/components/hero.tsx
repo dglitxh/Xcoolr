8823062c61fbd7675dcabc427d82258a931c08a5
ydeezly <ydglitch@gmail.com>
2022-11-23 08:23:52 +0000

    hero: upd layout

--- client/src/components/hero.tsx
+++ client/src/components/hero.tsx
@@ -4,21 +4,21 @@ import React from "react";
 export default function Hero() {
   return (
     <Container sm>
-      {/* <Grid.Container>
-        <Grid xs={12} sm={6} md={6} lg={6} justify="center"> */}
-      <Row>
-        <Text h2 b size={65}>
-          The Xcoolr Client
-        </Text>
-      </Row>
-      <Row>
-        <Text>
-          provides you with the most comprehensive student performance
-          monitoring platform on the market. We are know to deliver insightful
-          and in depth understanding of student's performance in the most
-          interactive way possible.
-        </Text>
-      </Row>
+      <div className="hero">
+        <Row>
+          <Text h2 b color={"blue"} size={65}>
+            The Xcoolr Client
+          </Text>
+        </Row>
+        <Row>
+          <Text size={25}>
+            provides you with the most comprehensive student performance
+            monitoring platform on the market. We are known to deliver
+            insightful and in-depth understanding of student's performance in
+            the most interactive way possible.
+          </Text>
+        </Row>
+      </div>
     </Container>
   );
 }
