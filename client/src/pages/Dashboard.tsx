8f13aadc2d73bbe01927abcbc350f80b6179af28
ydeezly <ydglitch@gmail.com>
2022-12-14 23:02:20 +0000

    upd: dash

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,9 +1,19 @@
+import { Col, Container, Text } from "@nextui-org/react";
 import React from "react";
-
 
 export default function Dashboard() {
   return (
     <div>
+      <Col span={3} css={{ backgroundColor: "gray" }}>
+        <Container>
+          <Text size={22} b h1>
+            {" "}
+            Xcooler Dash
+          </Text>
+          <Text></Text>
+        </Container>
+      </Col>
+      <Col span={9}></Col>
     </div>
   );
 }
